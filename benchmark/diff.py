05f1637a55b8824b470fa7041bde0806e68b244a
adamskrodzki <adam@oazoapps.com>
2023-12-25 20:37:54 +0100

    aider: Added script to compare benchmarks and output winning benchmark for each test.
    # Aider chat conversation:
    
    USER: I want to create python script capable of comparing two benchmarks side by side, I want to be able to invoke it as follows `benchmark\diff.py /path/to/benchmark1 /path/to/benchmark2 `  at that point script scans both folders for subfolder name, each subfolder name is a name of a test and both benchmark folders should have exacly the same list of subfolders. once list of subfolders is extracted scripts read .aider.results.json in each test subfolder.  each file is a single json object. for each result file script checks value of "test_outcomes" property  which should be array of 1 or more True/False values. length of an array can be different for different benchmarks. script should output in a new line, for each test: 1) name of a test, path of winning benchmark folder. benchmark  is winning if for a given test there is `true` value in tests_outcomes array before there is true value in other benchmark. for example if benchmark for a given test has array [false, true] and benchmark2 has array [false,false] benchmark1 wins and should be outputed, if arrays are [false,true] [false,true] it is a tie and particular testcase should have no output, same for arrays [false, false], [false,false]. Please implement script fully from given specification
    
    ASSISTANT: ```diff
    --- /dev/null
    +++ benchmark/diff.py
    @@ ... @@
    +import json
    +import os
    +import sys
    +
    +def read_results(path):
    +    with open(path, 'r') as file:
    +        return json.load(file)
    +
    +def compare_benchmarks(benchmark1, benchmark2):
    +    tests1 = set(os.listdir(benchmark1))
    +    tests2 = set(os.listdir(benchmark2))
    +
    +    if tests1 != tests2:
    +        raise ValueError("Benchmark directories do not have the same set of tests.")
    +
    +    for test in sorted(tests1):
    +        result1_path = os.path.join(benchmark1, test, '.aider.results.json')
    +        result2_path = os.path.join(benchmark2, test, '.aider.results.json')
    +
    +        result1 = read_results(result1_path)['test_outcomes']
    +        result2 = read_results(result2_path)['test_outcomes']
    +
    +        for outcome1, outcome2 in zip(result1, result2):
    +            if outcome1 and not outcome2:
    +                print(f"{test}: {benchmark1}")
    +                break
    +            elif outcome2 and not outcome1:
    +                print(f"{test}: {benchmark2}")
    +                break
    +
    +if __name__ == '__main__':
    +    if len(sys.argv) != 3:
    +        print("Usage: diff.py /path/to/benchmark1 /path/to/benchmark2")
    +        sys.exit(1)
    +
    +    benchmark1, benchmark2 = sys.argv[1], sys.argv[2]
    +    compare_benchmarks(benchmark1, benchmark2)
    ```

---
+++ benchmark/diff.py
@@ -0,0 +1,37 @@
+import json
+import os
+import sys
+
+def read_results(path):
+    with open(path, 'r') as file:
+        return json.load(file)
+
+def compare_benchmarks(benchmark1, benchmark2):
+    tests1 = set(os.listdir(benchmark1))
+    tests2 = set(os.listdir(benchmark2))
+
+    if tests1 != tests2:
+        raise ValueError("Benchmark directories do not have the same set of tests.")
+
+    for test in sorted(tests1):
+        result1_path = os.path.join(benchmark1, test, '.aider.results.json')
+        result2_path = os.path.join(benchmark2, test, '.aider.results.json')
+
+        result1 = read_results(result1_path)['test_outcomes']
+        result2 = read_results(result2_path)['test_outcomes']
+
+        for outcome1, outcome2 in zip(result1, result2):
+            if outcome1 and not outcome2:
+                print(f"{test}: {benchmark1}")
+                break
+            elif outcome2 and not outcome1:
+                print(f"{test}: {benchmark2}")
+                break
+
+if __name__ == '__main__':
+    if len(sys.argv) != 3:
+        print("Usage: diff.py /path/to/benchmark1 /path/to/benchmark2")
+        sys.exit(1)
+
+    benchmark1, benchmark2 = sys.argv[1], sys.argv[2]
+    compare_benchmarks(benchmark1, benchmark2)
